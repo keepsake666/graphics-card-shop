ed90f1d1436350a4e934c82216a1cacc41221e8d
Sergey Veselov <veselovsa@list.ru>
2023-09-06 14:37:41 +0900

    feat: "added header"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Logo } from "./ui/logo/logo";
 import { Header } from "./ui/header/header";
 
 function App() {
   return (
     <div>
-      <Logo />
       <Header />
     </div>
   );
--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,3 +1,27 @@
+import styles from "./styles.module.css";
+import { Logo } from "../logo/logo";
+
 export const Header = () => {
-  return <div> Header</div>;
+  return (
+    <div className={styles.header}>
+      <Logo />
+      <ul className={styles.list}>
+        <li className={styles.list__item}>
+          {" "}
+          <div>icon</div>
+          <p>Якутск</p>
+        </li>
+        <li className={styles.list__item}>
+          {" "}
+          <div>icon</div>
+          <p>Избранное</p>
+        </li>
+        <li className={styles.list__item}>
+          {" "}
+          <div>icon</div>
+          <p>Личный кабинет </p>
+        </li>
+      </ul>
+    </div>
+  );
 };
