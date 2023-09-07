b0b0180fc5d05a122dd695eda3edfc2df12c56ac
Sergey Veselov <veselovsa@list.ru>
2023-09-07 15:24:45 +0900

    feat: "начал делать кнопку"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./ui/header/header";
+import { Button } from "./ui/botton /botton";
 
 function App() {
   return (
     <div>
       <Header />
+      <Button text="Найтasdasdasdasdи" />
     </div>
   );
 }
