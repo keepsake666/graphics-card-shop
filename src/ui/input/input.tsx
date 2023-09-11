8a5a2432e26862c3396f76c9db71d4e3b32cf49b
Sergey Veselov <veselovsa@list.ru>
2023-09-11 11:37:14 +0900

    feat: "добавил input"

---
+++ src/ui/input/input.tsx
@@ -0,0 +1,65 @@
+import React, { useMemo } from "react";
+import cn from "classnames";
+import styles from "./styles.module.css";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  name: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label?: string;
+  extClassName?: string;
+  error?: boolean;
+  errorText?: string;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      value,
+      type,
+      name,
+      onChange,
+      label,
+      extClassName,
+      placeholder,
+      error,
+      errorText,
+      ...props
+    },
+    ref
+  ) => {
+    const errorToRender = useMemo(
+      () =>
+        error && errorText ? (
+          <span className={cn(styles.error, "text")}>{errorText}</span>
+        ) : null,
+      [error, errorText]
+    );
+
+    const inputClass = error ? styles.input_error : styles.input;
+
+    return (
+      <div className={extClassName}>
+        {label && (
+          <label className={cn(styles.label, "text")} htmlFor="2">
+            {label}
+          </label>
+        )}
+        <div className={styles.container}>
+          <input
+            ref={ref}
+            type={type}
+            name={name}
+            value={value}
+            className={cn(inputClass, "text", "text_size_medium")}
+            onChange={onChange}
+            placeholder={placeholder}
+            id="1"
+            {...props}
+          />
+          {errorToRender}
+        </div>
+      </div>
+    );
+  }
+);
