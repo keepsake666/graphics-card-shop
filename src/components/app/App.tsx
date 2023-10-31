7610799e6a54523a7dd6e4191b66889965ef2a7f
Sergey Veselov <veselovsa@list.ru>
2023-10-31 16:36:28 +0900

    feat: "product-page настройка роута"

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -8,60 +8,8 @@ import ErrorPage from "../../pages/error-page/error-page";
 
 function App() {
   const [footerInput, setFooterInput] = useState("");
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const fakeProducts = [
-    {
-      id: 1,
-      img: "img1.jpg",
-      company: "Apple",
-      name: "iPhone 12",
-      memory: "128GB",
-      price: 999,
-      shop: [
-        {
-          id: 1,
-          name: "Shop A",
-          model: "Model A",
-          price: 999,
-          link: "shopA.com",
-        },
-        {
-          id: 2,
-          name: "Shop B",
-          model: "Model B",
-          price: 899,
-          link: "shopB.com",
-        },
-      ],
-    },
-    {
-      id: 2,
-      img: "img2.jpg",
-      company: "Samsung",
-      name: "Galaxy S20",
-      memory: "256GB",
-      price: "1099",
-      shop: [
-        {
-          id: 3,
-          name: "Shop C",
-          model: "Model C",
-          price: "1099",
-          link: "shopC.com",
-        },
-        {
-          id: 4,
-          name: "Shop D",
-          model: "Model D",
-          price: "999",
-          link: "shopD.com",
-        },
-      ],
-    },
-  ];
-
   return (
-    <div>
+    <main>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -73,7 +21,7 @@ function App() {
         onChange={(e) => setFooterInput(e.target.value)}
         value={footerInput}
       />
-    </div>
+    </main>
   );
 }
 
--- src/pages/products-page/productsPage.tsx
+++ src/pages/products-page/productsPage.tsx
@@ -1,3 +1,91 @@
+import { useState } from "react";
+import styles from "./styles.module.css";
+import { Input } from "../../ui/input/input";
+import { Button } from "../../ui/botton/button";
+import { Catalog } from "../../ui/catalog/catalog";
+import { Product } from "../../components/product/product";
+import { Characteristics } from "../../ui/characteristics/characteristics";
+
 export const ProductsPage = () => {
-  return <div>Products Page</div>;
+  const fakeProducts = [
+    {
+      id: 1,
+      img: "img1.jpg",
+      company: "Apple",
+      name: "iPhone 12",
+      memory: "128GB",
+      price: 999,
+      shop: [
+        {
+          id: 1,
+          name: "Shop A",
+          model: "Model A",
+          price: 999,
+          link: "shopA.com",
+        },
+        {
+          id: 2,
+          name: "Shop B",
+          model: "Model B",
+          price: 899,
+          link: "shopB.com",
+        },
+      ],
+    },
+    {
+      id: 2,
+      img: "img2.jpg",
+      company: "Samsung",
+      name: "Galaxy S20",
+      memory: "256GB",
+      price: "1099",
+      shop: [
+        {
+          id: 3,
+          name: "Shop C",
+          model: "Model C",
+          price: "1099",
+          link: "shopC.com",
+        },
+        {
+          id: 4,
+          name: "Shop D",
+          model: "Model D",
+          price: "999",
+          link: "shopD.com",
+        },
+      ],
+    },
+  ];
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <Catalog />
+      <div className={styles.containerInput}>
+        <Input
+          extClassName={styles.input}
+          placeholder="Поиск товара"
+          value={value}
+          name="product"
+          onChange={(e) => setValue(e.target.value)}
+        />{" "}
+        <Button extClassName={styles.button} text="Найти" size="small" />
+      </div>
+      <div className={styles.containerProducts}>
+        <Characteristics />
+        {fakeProducts.map((item) => (
+          <Product
+            key={item.id}
+            id={item.id}
+            img={item.img}
+            company={item.company}
+            name={item.name}
+            memory={item.memory}
+            price={item.price}
+            shop={item.shop}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
